a5366b3495de28914a13c23c2b835aa80ebba3b3
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-01-01 23:12:58 +0100

    feed

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,68 +4,112 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 
-import Home from "Components/Pages/Home/Home";
-import { About } from "Components/Pages/About";
-import { Work } from "Components/Pages/Work";
-import {
-  WorkAlumniHub,
-  WorkFemi,
-  WorkFudtraka,
-  WorkLahdaa,
-  WorkTeleSchool,
-} from "Components/Pages/Works";
-import { Services } from "Components/Pages/Services";
+import React, { Suspense } from "react";
+
+const Home = React.lazy(() => import("Components/Pages/Home/Home"));
+const About = React.lazy(() => import("Components/Pages/About/About"));
+const Work = React.lazy(() => import("Components/Pages/Work/Work"));
+const WorkAlumniHub = React.lazy(
+  () => import("Components/Pages/Works/WorkAlumniHub")
+);
+const WorkFemi = React.lazy(() => import("Components/Pages/Works/WorkFemi"));
+const WorkFudtraka = React.lazy(
+  () => import("Components/Pages/Works/WorkFudtraka")
+);
+const WorkLahdaa = React.lazy(
+  () => import("Components/Pages/Works/WorkLahdaa")
+);
+const WorkTeleSchool = React.lazy(
+  () => import("Components/Pages/Works/WorkTeleSchool")
+);
+const Services = React.lazy(() => import("Components/Pages/Services/Services"));
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route
         path="/"
-        element={<Home />}
+        element={
+          <Suspense fallback={"Lodaing..."}>
+            <Home />
+          </Suspense>
+        }
         errorElement={<h2>Can't find this page</h2>}
       />
       <Route
         path="about"
-        element={<About />}
+        element={
+          <Suspense fallback={"Lodaing..."}>
+            <About />
+          </Suspense>
+        }
         errorElement={<h2>Can't find this page</h2>}
       />
       <Route
         path="services"
-        element={<Services />}
+        element={
+          <Suspense fallback={"Lodaing..."}>
+            <Services />
+          </Suspense>
+        }
         errorElement={<h2>Can't find this page</h2>}
       />
       <Route
         path="work"
-        element={<Work />}
+        element={
+          <Suspense fallback={"Lodaing..."}>
+            <Work />
+          </Suspense>
+        }
         errorElement={<h2>Can't find this page</h2>}
       />
 
       <Route
         path="work/lahdaa"
-        element={<WorkLahdaa />}
+        element={
+          <Suspense fallback={"Lodaing..."}>
+            <WorkLahdaa />
+          </Suspense>
+        }
         errorElement={<h2>Can't find this page</h2>}
       />
 
       <Route
         path="work/alumnihub"
-        element={<WorkAlumniHub />}
+        element={
+          <Suspense fallback={"Lodaing..."}>
+            <WorkAlumniHub />
+          </Suspense>
+        }
         errorElement={<h2>Can't find this page</h2>}
       />
 
       <Route
         path="work/teleschool"
-        element={<WorkTeleSchool />}
+        element={
+          <Suspense fallback={"Lodaing..."}>
+            <WorkTeleSchool />
+          </Suspense>
+        }
         errorElement={<h2>Can't find this page</h2>}
       />
 
       <Route
         path="work/fudtraka"
-        element={<WorkFudtraka />}
+        element={
+          <Suspense fallback={"Lodaing..."}>
+            <WorkFudtraka />
+          </Suspense>
+        }
         errorElement={<h2>Can't find this page</h2>}
       />
       <Route
         path="work/femi"
-        element={<WorkFemi />}
+        element={
+          <Suspense fallback={"Lodaing..."}>
+            <WorkFemi />
+          </Suspense>
+        }
         errorElement={<h2>Can't find this page</h2>}
       />
     </>
